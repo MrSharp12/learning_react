b8928920da541ef9e23b6e9c3aa109b49281597a
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-02-19 12:06:52 -0500

    normalize events with react synthentic event system

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-//adding custom propType validation to React components
+//normalize events with react synthetic event system
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { currentEvent: '---' };
+    this.update = this.update.bind(this)
+  }
+
+  update(e) {
+    this.setState({ currentEvent: e.type })
+  }
+
   render() {
-    return <Title text="Mason Storm"/>
+    return (
+      <div>
+        <textarea
+          onKeyPress={this.update}
+          onCopy={this.update}
+          onCut={this.update}
+          onPaste={this.update}
+          onFocus={this.update}
+          onBlur={this.update}
+          cols="30"
+          rows="10" />
+        <h1>{this.state.currentEvent}</h1>
+      </div>
+    );
   }
 }
 
-  const Title = (props) => <h1>Title: {props.text}</h1>
 
-  Title.propTypes = {
-    text(props, propName, component) {
-      if(!(propName in props)) {//means the prop name has not been provided
-        return new Error(`missing ${propName}`);
-      }
-      if(props[propName].length < 6) {
-        return new Error(`${propName} is too short.`)
-      }
-    }
-  }
 
 export default App;
