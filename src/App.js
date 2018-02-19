db8de4f70bd012ce0548d90a9a179fccf5a108fa
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-02-19 11:16:02 -0500

    access nested data

--- src/App.js
+++ src/App.js
@@ -1,33 +1,20 @@
+//access nested data with React props.children
 import React from 'react';
 
-//state is meant to be passed and updated by the component
-class App extends React.Component {//always render in a class component that returns DOM representation of our component
-  constructor() {
-    super();//super gives keyword this the context within our component rather than it's parent class
-    this.state = {
-      txt: 'Get em Sticks!'
-    }
-  }
-  update(e) {
-    this.setState({ txt: e.target.value })
-  }
+class App extends React.Component {
   render() {
-    return (//only allowed to return a single node, adjacent JSX must be wrapped in an enclosing tag 
-      <div>
-        <h1>{this.state.txt}</h1>
-        <Widget update={this.update.bind(this)} />
-      </div>
-    );
+    return <Button>I <Heart /> React</Button>
   }
 }
 
-//passing this in as a prop, when we type the child component will update the parent component
-const Widget = (props) =>
-  <input type="text" onChange={props.update} />
 
+//stateless Function
+const Button = (props) => <button>{props.children}</button>
 
-App.defaultProps = {
-  txt: 'And you can take that to the bank.'
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
 }
 
 export default App;
