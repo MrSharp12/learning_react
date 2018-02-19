40f22d9082f994775546fafbbf01268832e49c0a
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-02-19 14:26:10 -0500

    use React ref to get a Reference to specific components

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-//normalize events with react synthetic event system
+//use React ref to get a Reference to specific components
+//to to reference a node or specific import
 import React from 'react';
+
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = { currentEvent: '---' };
-    this.update = this.update.bind(this)
+    this.state = {a: ''}
   }
-
   update(e) {
-    this.setState({ currentEvent: e.type })
+    this.setState({
+      a: this.a.refs.input.value,//a is our component, input is our input field
+      b: this.refs.b.value//ref returns back the node we are referrencing
+    })//value from our input field
   }
-
   render() {
     return (
       <div>
-        <textarea
-          onKeyPress={this.update}
-          onCopy={this.update}
-          onCut={this.update}
-          onPaste={this.update}
-          onFocus={this.update}
-          onBlur={this.update}
-          cols="30"
-          rows="10" />
-        <h1>{this.state.currentEvent}</h1>
+        <Input
+          ref={ component => this.a = component}//example of a callback
+          update={this.update.bind(this)}
+        /> {this.state.a}
+        <hr />
+        <input
+          ref="b"
+          type="text"
+          onChange={this.update.bind(this)}
+        /> {this.state.b}
       </div>
     );
   }
 }
 
-
+class Input extends React.Component {
+  render() {
+    return <div><input ref="input" type="text" onChange={this.props.update} /></div>
+  }
+}
 
 export default App;
