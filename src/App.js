920d7c6f23e4a2864f5e7484bfd6b48b5d0b3212
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-02-20 22:26:00 -0500

    map work with React

--- src/App.js
+++ src/App.js
@@ -1,44 +1,27 @@
-//updating lifecycle methods in our components
-
+//using map to create react components
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {increasing: false};//determine if the new prop of val is increasing from the previous prop
+    this.state = {items: []};
   }
-  update() {
-    ReactDOM.render(
-    <App val={this.props.val + 1}/>, 
-    document.getElementById('root'))
+  componentWillMount() {
+    fetch('https://swapi.co/api/people/?format=json')
+      .then(response => response.json())
+      .then(({results: items}) => this.setState({items}))
   }
-
-  componentWillReceiveProps(nextProps) {//this means new properties are coming in
-    //we get access to those props from nextProps
-    this.setState({increasing: nextProps.val > this.props.val}) 
-
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    return nextProps.val % 5 === 0;
-  }
-
   render() {
-    console.log(this.state.increasing);//first time through should not be true, every other render it should be
-    return(
-      <button onClick={this.update.bind(this)}>
-        {this.props.val}
-      </button>
+    let items = this.state.items
+    //for each item the map iterates over
+    //place that item inbetween h4 tags
+    return (
+      <div>
+        {items.map(item => <h4 key={item.name}>{item.name}</h4>)}
+      </div>
     )
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    console.log(`prevProps: ${prevProps.val}`);
-  }
-
-} 
-
-App.defaultProps = {val: 0}
+}
 
 export default App
