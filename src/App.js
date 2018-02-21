c041e6aa28307e34a2635770d6c5940838465d67
The Sharps <Sharp@Thes-Air.attlocal.net>
2018-02-20 21:20:42 -0500

    managing component state with lifecycle methods

--- src/App.js
+++ src/App.js
@@ -15,16 +15,24 @@ class App extends React.Component {
   }
   componentWillMount() {//fires right before it's mounted into the DOM, et's us know it guarantees it gets tot the DOM
     console.log('component will mount');
+    this.setState({m: 2});
   }
   render() {
     console.log('render');
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>
+    {this.state.val * this.state.m}
+    </button>
   }
   componentDidMount() {//fires once component has been mounted to the DOM
+    //here we have access to the DOM
     console.log('component did mount');
+    this.inc = setInterval(this.update, 500);
   }
   componentWillUnmount() {//fires once component is about to leave the DOM
+    //we have access to states and props but do not have access to the DOM because it hasn't been placed in the DOM
+    //here we can clean up any running processes that we might need to
     console.log('component will unmount');
+    clearInterval(this.inc);
   }
 }
 
