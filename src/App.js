5709306fbeb52b72d29df8d668613a4a541a1bb4
The Sharps <Sharp@Thes-MacBook-Air.local>
2018-02-21 07:54:24 -0500

    higher order components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,60 @@
-//using map to create react components
+//higher order components
+//puprose of a higher order component is to share functionality
+//between multiple components
+//higher order components will take in a component and return a component
 import React from 'react';
 
-class App extends React.Component {
+const HOC = (InnerComponent) => class extends React.Component {
   constructor() {
     super();
-    this.state = {items: []};
+    this.state = {count: 0}
+  }
+  update() {
+    this.setState({count: this.state.count + 1})
   }
   componentWillMount() {
-    fetch('https://swapi.co/api/people/?format=json')
-      .then(response => response.json())
-      .then(({results: items}) => this.setState({items}))
+    console.log('will mount');
   }
   render() {
-    let items = this.state.items
-    //for each item the map iterates over
-    //place that item inbetween h4 tags
     return (
+      <InnerComponent
+        {...this.props}
+        {...this.state}
+        update={this.update.bind(this)}
+      />
+    )
+  }
+};
+//props children are no longer passing through
+//to fix this, spread this.props into our component
+class App extends React.Component {
+  render() {
+    return(
       <div>
-        {items.map(item => <h4 key={item.name}>{item.name}</h4>)}
+        <Button>button</Button>
+        <hr />
+        <LabelHOC>label</LabelHOC>
       </div>
     )
   }
-
 }
+
+const Button = HOC((props) => 
+  <button onClick={props.update}>{props.children} - {props.count}</button>)
+
+class Label extends React.Component {
+  componentWillMount() {
+    console.log('label will mount');
+  }
+  render() {
+    return (
+      <lable onMouseMove={this.props.update}>
+      {this.props.children} - {this.props.count}
+      </lable>
+    )
+  }
+}
+
+const LabelHOC = HOC(Label);
 
 export default App
